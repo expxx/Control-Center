d3c78a8010b05dc5e3ba194c801d173964a27a97
Cam <chrlew082@gmail.com>
2024-10-27 14:44:19 -0600

    resolve method for placeholders

--- src/main/java/dev/expx/ctrlctr/center/papi/PAPIAPI.java
+++ src/main/java/dev/expx/ctrlctr/center/papi/PAPIAPI.java
@@ -1,6 +1,7 @@
 package dev.expx.ctrlctr.center.papi;
 
 import dev.expx.ctrlctr.center.Ctrlctr;
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +25,17 @@ public class PAPIAPI {
     public static void register(String id, PAPIExpansion.PlaceholderHandler<Player, String> handler) {
         LoggerFactory.getLogger(PAPIAPI.class).info(Ctrlctr.getLang().lang("placeholder-register", id));
         PAPIExpansion.handlers.register(id, handler);
+    }
+
+    /**
+     * Resolves a placeholder from a
+     * player using PlaceholderAPI
+     * @param player Player
+     * @param placeholder Placeholder
+     * @return Resolved placeholder
+     */
+    public static String resolve(Player player, String placeholder) {
+        return PlaceholderAPI.setPlaceholders(player, placeholder);
     }
 
 }
