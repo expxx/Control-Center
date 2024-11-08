5e5818d2f3c29f5b250a9a179a92681baa6c6af1
Cam <chrlew082@gmail.com>
2024-11-08 15:16:39 -0700

    add a ItemUtil#getHeadFromTexture method

--- src/main/java/dev/expx/ctrlctr/center/util/ItemUtil.java
+++ src/main/java/dev/expx/ctrlctr/center/util/ItemUtil.java
@@ -1,10 +1,21 @@
 package dev.expx.ctrlctr.center.util;
 
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
 import com.google.gson.Gson;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.Field;
 import java.util.Base64;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Item utility.
@@ -39,6 +50,23 @@ public class ItemUtil {
         String json = new String(Base64.getDecoder().decode(serialized));
         @SuppressWarnings("unchecked") Map<String, Object> map = gson.fromJson(json, Map.class);
         return ItemStack.deserialize(map);
+    }
+
+    /**
+     * Retrieve a PlayerHead object from a
+     * Base64 head texture.
+     * @param texture Base64 encoded texture
+     * @return PlayerHead ItemStack
+     */
+    public static ItemStack getHeadFromTexture(String texture) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        PlayerProfile p = Bukkit.createProfile(UUID.randomUUID(), UUID.randomUUID().toString());
+        p.setProperty(new ProfileProperty("textures", texture));
+        meta.setOwnerProfile(p);
+        head.setItemMeta(meta);
+
+        return head;
     }
 
 }
